c2df089e40db35a1d757161024fa36d06ee1a970
Egor <edkuz11@yandex.ru>
2022-05-21 22:02:14 +0300

    Get zipp and add zipp

--- internals/src/func.py
+++
@@ -1,8 +0,0 @@
-import os
-import os.path as path
-
-def get_root_dir():
-    cwd = os.getcwd()
-    # Get root dir (2 lvl up)
-    root_dir = path.join(path.abspath(path.join(cwd, "../..")), )
-    return root_dir
